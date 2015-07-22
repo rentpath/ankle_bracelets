42376060fa6b4d8278b77f340d800c6a4bac18f6
Hunter Haydel <haydh530@gmail.com>
2015-07-22 18:30:28 -0500

    Fix misspelled route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'organiztions#index' #TODO create homepage
+  root 'organizations#index' #TODO create homepage
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
