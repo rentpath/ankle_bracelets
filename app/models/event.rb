d005be7d0e9418038e9936b59dec6b23b8ea5e7e
Hunter Haydel <haydh530@gmail.com>
2015-07-23 12:36:27 -0500

    Derp, that belongs in shift

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,9 +2,4 @@ class Event < ActiveRecord::Base
   belongs_to :organization
   has_and_belongs_to_many :volunteers, class_name: 'User'
   has_many :shifts
-
-  validates :title, presence: true
-  validates :slots, presence: true, numericality: { only_integer: true }
-  validates :start_time, presence: true
-  validates :end_time, presence: true
 end
--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,3 +1,8 @@
 class Shift < ActiveRecord::Base
   belongs_to :event
+
+  validates :title, presence: true
+  validates :slots, presence: true, numericality: { only_integer: true }
+  validates :start_time, presence: true
+  validates :end_time, presence: true
 end
