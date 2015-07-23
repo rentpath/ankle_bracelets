3f1e28587c75a4c60c3559aa033992061505c925
Hunter Haydel <haydh530@gmail.com>
2015-07-23 17:52:09 -0500

    Add address partial

--- app/helpers/organizations_helper.rb
+++ app/helpers/organizations_helper.rb
@@ -1,9 +1,2 @@
 module OrganizationsHelper
-  def full_address
-    "#{@organization.address1} #{@organization.address2}"
-  end
-
-  def city_state_zip
-    "#{@organization.city}, #{@organization.state} #{@organization.zip}"
-  end
 end
