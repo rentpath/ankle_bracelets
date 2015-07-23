2fd9a90a1c86b63063f8f63a711d188269b642a8
Hunter Haydel <haydh530@gmail.com>
2015-07-23 17:21:39 -0500

    When logging in return to last page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  after_filter :store_location
+  def store_location
+    if (request.fullpath != "/users/sign_in" &&
+          request.fullpath != "/users/sign_up" &&
+          request.fullpath != "/users/password" &&
+          request.fullpath != "/users/sign_out" &&
+          !request.xhr?) # don't store ajax calls
+       store_location_for :user, request.fullpath 
+    end
+  end
 end
