74084a27752752a09bf9b1cbddd06209ebe93a65
Matt Perryman <mperryman@primedia.com>
2015-07-23 17:12:12 -0500

    Organization Page Cleanup

--- app/helpers/organizations_helper.rb
+++ app/helpers/organizations_helper.rb
@@ -1,2 +1,9 @@
 module OrganizationsHelper
+  def full_address
+    "#{@organization.address1} #{@organization.address2}"
+  end
+
+  def city_state_zip
+    "#{@organization.city}, #{@organization.state} #{@organization.zip}"
+  end
 end
